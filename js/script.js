6d7c9b585347611a122f1f693b953792df7f64af
Robby94gtr <robertogramazio94@gmail.com>
2025-03-18 17:30:47 +0100

    [END] Created "if" (fixed previous order) that prints "FizzBuzz" instead of multiples of both 3 and 5

--- js/script.js
+++ js/script.js
@@ -12,18 +12,26 @@ Buon lavoro e buon divertimento! */
 // Creo for che stampi numeri da 1 a 100 
 
 for (let i = 1; i <= 100; i++)
-    
-// Creo if per i multipli di 3 "Fizz"
 
-if(i % 3 == 0){
-    console.log(`${i} Fizz`);
+// Creo if per i multipli di 3 e di 5 "FizzBuzz"
+
+if(i % 3 == 0 && i % 5 == 0){
+    console.log(`FizzBuzz`);
+}
+    
+// Creo else if per i multipli di 3 "Fizz"
+
+else if(i % 3 == 0){
+    console.log(`Fizz`);
 } 
 
 // Creo else if per i multipli di 5 "Buzz"
 
 else if(i % 5 == 0){
-    console.log(`${i} Buzz`);
+    console.log(`Buzz`);
 }
+
+
 
 else {
     console.log(i)
